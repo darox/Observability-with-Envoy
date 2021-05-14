e2805f74a524e413fa71e9bc76a5235cb02d3b51
Mader Dario, INI-MBM-MET <Dario.Mader@swisscom.com>
2021-05-14 19:39:57 +0200

    fixed some stuff of zipkin tracing

--- flask-app-1/src/__init__.py
+++ flask-app-1/src/__init__.py
@@ -3,10 +3,29 @@ import requests
 
 app = Flask(__name__)
 
+
+TRACE_HEADERS_TO_PROPAGATE = [
+    "X-Ot-Span-Context",
+    "X-Request-Id",
+
+    # Zipkin headers
+    "X-B3-TraceId",
+    "X-B3-SpanId",
+    "X-B3-ParentSpanId",
+    "X-B3-Sampled",
+    "X-B3-Flags",
+
+    "uber-trace-id",
+]
+
 @app.route('/', methods=["GET"])
 def index():
+    headers = {}
+    for header in TRACE_HEADERS_TO_PROPAGATE:
+        if header in request.headers:
+            headers[header] = request.headers[header]
     try:
-        response = requests.get("http://envoy-sidecar-flask-app-2/api/v1/status")
+        response = requests.get("http://envoy-sidecar-1:9090/api/v1/status", headers=headers)
     
         return jsonify(response.json())
     except:
