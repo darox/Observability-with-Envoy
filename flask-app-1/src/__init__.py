b60bf3fc6f4a28f899be8912b182b9e8462bafec
Dario Mader <maderdario@gmail.com>
2021-05-22 12:21:15 +0200

    added bootstrap

--- flask-app-1/src/__init__.py
+++ flask-app-1/src/__init__.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 import requests
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 TRACE_HEADERS_TO_PROPAGATE = [
@@ -24,15 +23,18 @@ def index():
     headers = {}
     for header in TRACE_HEADERS_TO_PROPAGATE:
         if header in request.headers:
-            headers[header] = request.headers[header]
+            headers[header] = request.headers[header]   
+    print(headers)            
+    
     try:
         response = requests.get("http://envoy-sidecar-1:9090/api/v1/status", headers=headers)
+        print(response.json())
 
-        return response.json(), 200
+        return render_template("index.html", response=response), 200
 
     except AssertionError as e:
+        return render_template("error.html"), 500
         print(e)
-        return "Internal Server Error", 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
