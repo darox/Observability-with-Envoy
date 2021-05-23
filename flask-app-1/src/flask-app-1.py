dbf57d08b402ad1889f456f36d7e0f26e8fb4e3b
Dario Mader <maderdario@gmail.com>
2021-05-23 17:46:16 +0200

    added sli for latency and availability

--- flask-app-1/src/flask-app-1.py
+++ flask-app-1/src/flask-app-1.py
@@ -4,11 +4,7 @@ import requests, json_logging, logging
 app = Flask(__name__)
 
 json_logging.init_flask(enable_json=True)
-logger = logging.getLogger("flask-app-1")
-logger.setLevel(logging.ERROR)
-handler = logging.handlers.RotatingFileHandler(filename='/log/error.log', maxBytes=5000000, backupCount=10)
-logger.addHandler(handler)
-
+json_logging.init_request_instrument(app)
 
 TRACE_HEADERS_TO_PROPAGATE = [
     "X-Ot-Span-Context",
@@ -26,22 +22,24 @@ TRACE_HEADERS_TO_PROPAGATE = [
 
 @app.route('/', methods=["GET"])
 def index():
+    
+    logger = logging.getLogger("flask-app-1")
+    logger.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(filename='/log/debug-flask-app-1.log', maxBytes=5000000, backupCount=10)
+    logger.addHandler(handler)
+
     headers = {}
     for header in TRACE_HEADERS_TO_PROPAGATE:
         if header in request.headers:
-            headers[header] = request.headers[header]   
-    print(headers)   
-    print(headers['X-B3-TraceId'])      
+            headers[header] = request.headers[header]    
     
     try:
         response = requests.get("http://envoy-sidecar-1:9090/api/v1/status", headers=headers)
-
+        logger.info("This request was handeled successfully")
         return render_template("index.html", response=response.json()), 200
 
     except AssertionError as e:
-        message = str(e)
-        traceid = headers['X-B3-TraceId']
-        logger.error(message, extra={'props': {'trace_id': traceid}})
+        logger.error("This request has failed")
         return 500
 
 if __name__ == "__main__":
